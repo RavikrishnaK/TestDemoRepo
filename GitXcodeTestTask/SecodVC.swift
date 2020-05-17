c9ec3c05233cbe78b4532cae952a1ea5bf7d2833
kravikrishna.33@gmail.com <kravikrishna.33@gmail.com>
2020-05-17 18:57:03 +0530

    added new controller

---
+++ GitXcodeTestTask/SecodVC.swift
@@ -0,0 +1,19 @@
+//
+//  SecodVC.swift
+//  GitXcodeTestTask
+//
+//  Created by Luoocy on 17/05/20.
+//  Copyright © 2020 Ravikrishna K. All rights reserved.
+//
+
+import UIKit
+
+class SecodVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("This is second screen")
+    }
+    
+}
